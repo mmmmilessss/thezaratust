e868df638dcb0f81101dbb1bf6cb0b931e58be9c
PARK GEONWOO <miles@Miless-MacBook-Pro.local>
2026-03-12 08:21:48 +0900

    update contact email

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,9 @@ export default function Footer() {
   return (
     <footer className="py-16 text-center text-[0.45rem] font-gotham-bold opacity-60 sm:text-xs">
       <p>© 2026 MILES ZARATUST. ALL RIGHTS RESERVED.</p>
-      <p>CONTACT@MILESZARATUST.COM</p>
+      <p>
+        <a href="mailto:contact@zaratust.com">CONTACT@ZARATUST.COM</a>
+      </p>
     </footer>
   );
 }
