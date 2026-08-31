5e07d1e0baceef2dc7878a2f7fd1a292bcc0af47
PARK GEONWOO <miles@Miless-MacBook-Pro.local>
2026-08-31 20:29:56 +0900

    Update footer copyright name

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="py-16 text-center text-[0.45rem] font-gotham-bold opacity-60 sm:text-xs">
-      <p>© 2026 MILES ZARATUST. ALL RIGHTS RESERVED.</p>
+      <p>© 2026 PARK GEON WOO. ALL RIGHTS RESERVED.</p>
       <p>
         <a href="mailto:contact@zaratust.com">CONTACT@ZARATUST.COM</a>
       </p>
